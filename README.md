27e5e9e3feebcdd8fe31a215277d32ce88b94640
Vipul K Sharma <116380760+sharma1256@users.noreply.github.com>
2024-02-28 10:58:49 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Safe-Reinforcement-Learning-for-Quadcopter
+Environments and algorithms for CBF-constrained policy optimization and projection based Safe RL Algorithm for Benchmark.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='gym_models',
+      version='0.0.1',
+      install_requires=['gym',
+                        'numpy',
+                        'matplotlib',
+                        'torch',
+                        'wesutils',
+                        'scipy',
+                        'ray',
+                        'pandas',
+                        'seaborn',
+                        'stable_baselines3',
+                        'math',
+                        'cvxpy',
+                        'cvxopt',
+                        'time',
+                        'warnings',
+                        'copy',
+                        'scipy',
+                        'csv',
+                        'datetime'
+                       ]
+)
