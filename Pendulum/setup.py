66e8eae5359b69be806ccb4067b9198262b899fa
Vipul K Sharma <116380760+sharma1256@users.noreply.github.com>
2024-02-29 21:45:02 -0500

    Add files via upload

---
+++ Pendulum/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='gym_models',
+      version='0.0.1',
+      author="Krishna Chaitanya Kosaraju and Wesley Suttle",
+      install_requires=['gym',
+                        'numpy',
+                        'matplotlib',
+                        'torch',
+                        'wesutils',
+                        'scipy',
+                        'ray',
+                        'pandas',
+                        'seaborn',
+                        'stable_baselines3',
+                       ]
+)
